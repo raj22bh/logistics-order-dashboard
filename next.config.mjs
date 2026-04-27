957388e41e6cb24c14b49fda0f79aff5fa11fa4f
Raj Bhalerao <147932766+raj22bh@users.noreply.github.com>
2026-04-27 11:51:11 +0530

    Remove basePath to fix Netlify deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   output: "export",
-  basePath: isProd ? "/logistics-order-dashboard" : "",
-  assetPrefix: isProd ? "/logistics-order-dashboard/" : "",
   images: {
     unoptimized: true,
   },
